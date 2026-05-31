4b065ed82164e0e6276602df669bec92a2873b9c
obrentanasic <obrentanasic@gmail.com>
2026-05-31 02:16:42 +0200

    Revert discrete inputs to stub (no digital input in RTU config)

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadDiscreteInputsFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadDiscreteInputsFunction.cs
@@ -24,40 +24,15 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            ModbusReadCommandParameters parameters = this.CommandParameters as ModbusReadCommandParameters;
-            byte[] request = new byte[12];
-
-            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parameters.TransactionId)), 0, request, 0, 2);
-            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parameters.ProtocolId)), 0, request, 2, 2);
-            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parameters.Length)), 0, request, 4, 2);
-            request[6] = parameters.UnitId;
-            request[7] = parameters.FunctionCode;
-            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parameters.StartAddress)), 0, request, 8, 2);
-            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parameters.Quantity)), 0, request, 10, 2);
-
-            return request;
+            //TO DO: IMPLEMENT
+            throw new NotImplementedException();
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            if ((response[7] & 0x80) != 0)
-            {
-                HandeException(response[8]);
-            }
-
-            Dictionary<Tuple<PointType, ushort>, ushort> pointValues = new Dictionary<Tuple<PointType, ushort>, ushort>();
-            ModbusReadCommandParameters parameters = this.CommandParameters as ModbusReadCommandParameters;
-
-            for (int i = 0; i < parameters.Quantity; i++)
-            {
-                int byteIndex = 9 + (i / 8);
-                int bitIndex = i % 8;
-                ushort bitValue = (ushort)((response[byteIndex] & (1 << bitIndex)) != 0 ? 1 : 0);
-                pointValues.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_INPUT, (ushort)(parameters.StartAddress + i)), bitValue);
-            }
-
-            return pointValues;
+            //TO DO: IMPLEMENT
+            throw new NotImplementedException();
         }
     }
 }
